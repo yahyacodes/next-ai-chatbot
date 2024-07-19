5e59992523de09093017fd98eb833fd9486a918e
yahyacodes <yahya.codin@gmail.com>
2024-07-19 15:56:18 +0300

    feat: Ai Chatbot with Gemini

--- components/chat.tsx
+++ components/chat.tsx
@@ -7,12 +7,6 @@ import { Button } from "./ui/button";
 import { SendHorizontalIcon, User } from "lucide-react";
 import { ScrollArea } from "./ui/scroll-area";
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
-import {
-  Tooltip,
-  TooltipTrigger,
-  TooltipProvider,
-  TooltipContent,
-} from "@radix-ui/react-tooltip";
 import { Textarea } from "./ui/textarea";
 
 const Chat = () => {
@@ -33,6 +27,13 @@ const Chat = () => {
     ref.current.scrollTo(0, ref.current.scrollHeight);
   }, [messages]);
 
+  const handleKeyDown = (event: any) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      handleSubmit(event);
+    }
+  };
+
   return (
     <section className=" text-zinc-700 p-2">
       <div className="mx-auto flex flex-col items-center justify-center">
@@ -44,15 +45,16 @@ const Chat = () => {
             {messages.map((m) => (
               <div key={m.id} className="mr-6 whitespace-pre-wrap md:mr-12">
                 {m.role === "user" && (
-                  <div className="mb-6 flex gap-3">
-                    <Avatar>
-                      <User className="rounded-full bg-zinc-300" />
-                    </Avatar>
+                  <div className="mb-6 flex justify-end gap-3">
                     <div>
-                      <p className="font-semibold">You</p>
-                      <div className="mt-1.5 text-sm text-zinc-500">
+                      <div className="text-md text-zinc-100 bg-zinc-900 p-2 rounded-md">
                         {m.content}
                       </div>
+                    </div>
+                    <div className="mt-1">
+                      <Avatar>
+                        <User className="rounded-full bg-zinc-300 h-8 w-8" />
+                      </Avatar>
                     </div>
                   </div>
                 )}
@@ -65,13 +67,11 @@ const Chat = () => {
                         alt="@shadcn"
                         className="rounded-full h-8 w-8 border"
                       />
-                      <AvatarFallback>CN</AvatarFallback>
+                      <AvatarFallback>G</AvatarFallback>
                     </Avatar>
                     <div className=" w-full">
-                      <div className="flex justify-between">
-                        <p className="font-semibold">Bot</p>
-                      </div>
-                      <div className="mt-2 text-sm text-zinc-700 bg-zinc-200 p-2 rounded-md">
+                      <div className="flex justify-between"></div>
+                      <div className=" text-md text-zinc-700 bg-zinc-200 p-2 rounded-md">
                         {m.content}
                       </div>
                     </div>
@@ -85,11 +85,13 @@ const Chat = () => {
             onSubmit={handleSubmit}
             className="container fixed bottom-4 z-100 flex flex-col items-center justify-center"
           >
-            <Input
+            <Textarea
               value={input}
               onChange={handleInputChange}
               placeholder="Enter a prompt here"
-              className="min-h-[60px] bg-zinc-700 w-full resize-none px-4 py-[1.3rem] focus-within:outline-none sm:text-sm placeholder:italic placeholder:text-zinc-50 text-zinc-50 focus-visible:ring-none"
+              rows={1}
+              onKeyDown={handleKeyDown}
+              className="min-h-[60px] bg-zinc-900 w-full resize-none px-4 py-[1.3rem] focus-within:outline-none sm:text-sm placeholder:italic placeholder:text-zinc-50 text-zinc-50 focus-visible:ring-zinc-900/40 overflow-hidden"
             />
             <div className="absolute right-0 top-[13px] sm:right-4">
               <Button
@@ -97,7 +99,7 @@ const Chat = () => {
                 type="submit"
                 variant="secondary"
                 disabled={isLoading}
-                className="absolute xl:right-8 right-10 top-0.8 h-8 w-10"
+                className="absolute xl:right-8 right-10 top-1 h-8 w-10"
               >
                 <SendHorizontalIcon className="h-5 w-5 text-zinc-500" />
               </Button>
